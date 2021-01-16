d69053dacb67cf96d7925be7c14c56b8bf60a6d7
cvpua <cvpua@up.edu.ph>
2021-01-16 17:19:49 +0800

    Populate home screen and make an add button in order screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,29 +1,91 @@
 import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    ScrollView,
+    Button    
+}
+from 'react-native';
 import 'react-native-gesture-handler';
 import Header from '../components/Header';
-
+import Pata from '../assets/food/pata.jpeg';
+import Chicken from '../assets/food/chicken.jpg';
+import Shanghai from '../assets/food/lumpia.jpg';
+import Carbonara from '../assets/food/carbonara.jpg';
+import FoodContainer from '../components/FoodContainer';
 
 const styles = StyleSheet.create({
     container: {
-      flex : 1,
-      justifyContent : 'center',
-      alignItems : 'center'
+        flex : 1,
+        justifyContent : 'center',
+        flexDirection :'row',
+        flexWrap : 'wrap',
+        // marginLeft : 10
     },
+    titlteContainer : {
+        alignItems : 'center',
+        margin : 20
+    },
+    titleStyle : {
+        fontSize : 32,
+        
+    }
   });
 
 const Home = (props) => {
     return(
         <View style = {{flex:1}}>
         <Header title = "Home" onTap = {props.navigation.openDrawer}/>
-        <View style = {styles.container}> 
-            <Text>
-                Home
-            </Text>
+            <View  style = {styles.titlteContainer}>
+            <Text style = {styles.titleStyle}>MENU</Text>
             </View>
+        <ScrollView>
+            <View style = {styles.container}> 
+            <FoodContainer 
+                foodImage = {Pata} 
+                foodName = "Crispy Pata"
+                foodPrice = "500"
+                foodDescription = "Lasang Pata Lasang Pata Lasang Pata"
+                foodQuantity = "1" />
+            <FoodContainer 
+                foodImage = {Chicken} 
+                foodName = "Roasted Chicken" 
+                foodPrice = "300"
+                foodDescription = "Lasang Manok Lasang Manok Lasang Manok Lasang Manok"
+                foodQuantity = "1"
+                />
+            <FoodContainer 
+                foodImage = {Shanghai} 
+                foodName = "Lumpiang Shanghai" 
+                foodPrice = "100"
+                foodDescription = "Lasang Shanghai"
+                foodQuantity = "12"
+                />
+            <FoodContainer 
+                foodImage = {Carbonara} 
+                foodName = "Carbonara" 
+                foodPrice = "750"
+                foodDescription = "Lasang Carbonara"
+                foodQuantity = "1"
+                />
+            <FoodContainer 
+                foodImage = {Pata} 
+                foodName = "Crispy Pata"
+                foodPrice = "500"
+                foodDescription = "Lasang Pata"
+                foodQuantity = "1" />
+            <FoodContainer 
+                foodImage = {Pata} 
+                foodName = "Crispy Pata"
+                foodPrice = "500"
+                foodDescription = "Lasang Pata"
+                foodQuantity = "1" />
+            </View>
+        </ScrollView>
         </View>
     )
 };
-
 
 export default Home;
--- screens/Order.js
+++ screens/Order.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity
+} from 'react-native';
 import Header from '../components/Header';
 
 const styles = StyleSheet.create({
     container: {
       flex : 1,
       justifyContent : 'center',
-      alignItems : 'center'
+      alignItems : 'center',
+    //   borderWidth : 1
     },
+    orderButton : {
+        position : 'absolute',
+        bottom : 0,
+        right : 0,
+        width : 60,
+        height : 60,
+        borderRadius : 60/2,
+        marginBottom : 40,
+        marginRight : 40,
+        // borderWidth : 1,
+        justifyContent : 'center',
+        alignItems : 'center',
+        backgroundColor : 'grey'
+    },
+
   });
 
 const Order = (props) => {
@@ -15,9 +36,11 @@ const Order = (props) => {
         <View style = {{flex:1}}>
         <Header title = "Orders" onTap = {props.navigation.openDrawer}/>
         <View style = {styles.container}> 
-            <Text>
-                Order
-            </Text>
+            <TouchableOpacity style = {styles.orderButton} >
+                <View>
+                <Text style = {{fontSize : 40}}>+</Text>
+                </View>
+            </TouchableOpacity>
             </View>
         </View>
     )
