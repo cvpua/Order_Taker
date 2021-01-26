5f18715e0d06d31ab00c9ad4fd89f51c5e72d2df
cvpua <cvpua@up.edu.ph>
2021-01-26 15:14:49 +0800

    Add date converter to pick time and date seperately

---
+++ converter/date-time-converter.js
@@ -0,0 +1,17 @@
+
+
+const Converter = (input,output) => {
+    
+    let result;
+    if (output === "date"){
+
+    }else if (output === "time"){
+
+    }
+
+    return result;
+
+};
+
+
+export default Converter;
